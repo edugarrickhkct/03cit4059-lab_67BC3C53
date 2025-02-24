5e8de948045abbec966fd286428197c941cf7d6c
Garrick Ho <sales@cherryec.com>
2025-02-24 21:21:19 +0800

    1st version.

---
+++ src/myhttps.py
@@ -0,0 +1,21 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+import sys
+
+def main():
+    port = 4443
+    if len(sys.argv) >= 2:
+        port = sys.argv[1]
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(
+        certfile="./cert.pem", 
+        keyfile="./key.pem")
+    context.check_hostname = False
+
+    with HTTPServer(("localhost", port), SimpleHTTPRequestHandler) as httpd:
+        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+        print("Listening on port: {}".format(port))
+        httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
